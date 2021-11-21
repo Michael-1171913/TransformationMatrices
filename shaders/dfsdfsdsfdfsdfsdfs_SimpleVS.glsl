373f997efc6a182cf735a0cb6c407fcc55c3e64e
Michael Leonard <michael.leonard@mymail.champlain.edu>
2021-11-21 01:30:48 -0500

    finished for now

--- shaders/dfsdfsdsfdfsdfsdfs_SimpleVS.glsl
+++ shaders/dfsdfsdsfdfsdfsdfs_SimpleVS.glsl
@@ -1,7 +1,6 @@
 #version 330
 
 uniform mat4 matVP;
-uniform mat4 matGeo;
 uniform vec3 cameraPos;
 uniform vec3 cameraRot;
 uniform float iTime;
@@ -9,6 +8,12 @@ uniform float iTime;
 uniform vec3 boxPos;
 uniform vec3 boxRot;
 uniform vec3 boxScale;
+
+uniform vec3 cameraPosition;
+
+uniform bool isPerspectiveProjection;
+uniform vec4 LeftRightTopBottom;
+uniform vec2 NearFar;
 
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
@@ -58,6 +63,63 @@ mat4 scaleMatrix(vec3 scaleBy)
 	);
 }
 
+mat4 lookatMatrix()
+{
+	// point of interest
+	vec3 p = vec3(0.f, 0.f, 0.f);
+	// up vector
+	vec3 u = vec3(0.f, 1.f, 0.f);
+	// eye
+	vec3 e = cameraPosition;
+
+	// forward vector
+	vec3 f = (p - e)/(abs(p-e));
+	
+	vec3 s = f * u;
+	vec3 uPrime = s * f;
+	
+	return mat4(
+	s.x, s.y, s.z, 0.f,
+	uPrime.x, uPrime.y, uPrime.z, 0.f,
+	f.x, f.y, f.z, 0.f,
+	-e.x, -e.y, -e.z, 1.f
+	);
+}
+
+mat4 projectionMatrix()
+{
+	float left = LeftRightTopBottom[0];
+	float right = LeftRightTopBottom[1];
+	float top = LeftRightTopBottom[2];
+	float bottom = LeftRightTopBottom[3];
+	float near = NearFar[0];
+	float far = NearFar[1];
+	
+	return mat4(
+	(2*near)/(right-left), 0.f, 0.f, 0.f,
+	0.f, (2*near)/(top-bottom), 0.f, 0.f,
+	(right+left)/(right-left), (top+bottom)/(top-bottom), (near+far)/(near-far), -1.f,
+	0.f, 0.f, (2*near*far)/(near-far), 0.f
+	);
+}
+
+mat4 orthographicMatrix()
+{
+	float left = LeftRightTopBottom[0];
+	float right = LeftRightTopBottom[1];
+	float top = LeftRightTopBottom[2];
+	float bottom = LeftRightTopBottom[3];
+	float near = NearFar[0];
+	float far = NearFar[1];
+	
+	return mat4(
+	2/(right-left), 0.f, 0.f, 0.f,
+	0.f, 2/(top-bottom), 0.f, 0.f,
+	0.f, 0.f, 2/(near-far), 0.f,
+	(left+right)/(left-right), (bottom+top)/(bottom-top), (near+far)/(near-far), 1.f
+	);
+}
+
 void main() {
    color = vec4(abs(normal), 1.0);
    float amount = 5;
@@ -65,8 +127,12 @@ void main() {
      scaleMatrix(boxScale) *
      rotationMatrix(boxRot) *
      translationMatrix(boxPos);
-   
-   gl_Position = matVP *
+  
+   //transformationMatrix *= lookatMatrix();
+  
+   mat4 perspectiveMatrix = isPerspectiveProjection ? projectionMatrix() : orthographicMatrix();
+  
+   gl_Position = perspectiveMatrix *
      transformationMatrix *
      vec4(pos, 1);
 }
